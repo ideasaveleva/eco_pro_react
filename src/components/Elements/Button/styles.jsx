c084cad69d087215a783866bb3b866804c825c6b
Katerina Saveleva <ash2789@mail.ru>
2023-05-04 12:11:31 -0600

    changed styles button

--- src/components/Elements/Button/styles.jsx
+++ src/components/Elements/Button/styles.jsx
@@ -1,21 +1,25 @@
 import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
-  font-family: 'Steppe';
+  font-family: 'Steppe', sans-serif;
   font-size: 20px;
-  padding: 20px 50px;
+  /* padding: 20px 50px; */
+  /* border-radius: 10px; */
+  padding: 14px 40px;
+  border-radius: 8px;
+
   background: #5da104;
   color: white;
-  border-radius: 10px;
   border: none;
   cursor: pointer;
   display: inline-block;
   white-space: nowrap;
 
   @media (max-width: 992px) {
-    padding: 12px 20px;
+    /* padding: 12px 20px; */
   }
   @media (max-width: 767px) {
     font-size: 12px;
+    padding: 10px 24px;
   }
 `
