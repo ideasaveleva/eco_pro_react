05c17a005e8bc467b4fcd54a9e538222ba32493b
Katerina Saveleva <ash2789@mail.ru>
2023-05-04 12:11:54 -0600

    changed styles input

--- src/components/Elements/Input/styles.jsx
+++ src/components/Elements/Input/styles.jsx
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 
 export const InputWrapper = styled.input`
   background: #c5d959;
-  border-radius: 10px;
+  border-radius: 8px;
   border: 1px solid #acc1ff;
   width: 100%;
   padding: 20px 30px;
   display: block;
   font-size: 16px;
   margin-bottom: 30px;
+  font-family: 'Steppe', sans-serif;
 
   @media (max-width: 992px) {
     font-size: 12px;
